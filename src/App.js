4c20de6944f7b6834704865faad59175ab9f74ea
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-20 14:27:34 +0300

    init useState & antd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import { Button, Flex } from "antd";
+
 import "./App.css";
 
 export default function App() {
-  let count = 0;
-  const increase = () => {count ++;}
-  const decrease = () => {count ++;}
+  const [total, setTotal] = useState(0);
+  const increase = () => {
+    setTotal(total + 1);
+  };
+  const decrease = () => {
+    setTotal(total - 1);
+  };
   return (
     <>
       <div className="App">
-        <p>{count}</p>
-        <button onClick={increase} >increase</button>
-        <button onClick={decrease} >decrease</button>
+        <Flex gap="small" wrap="wrap">
+          <p>{total}</p>
+        </Flex>
+        <Flex gap="small" wrap="wrap">
+          <Button type="primary" onClick={increase}>
+            Increase
+          </Button>
+          <Button type="primary" onClick={decrease}>
+            Decrease
+          </Button>
+        </Flex>
       </div>
     </>
   );
