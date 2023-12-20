f47126eef9072c4ea1bd1692badc96d16539e92e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-20 14:43:30 +0300

    init useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import { Button, Flex } from "antd";
+import { useEffect, useState } from "react";
+import { Card, Space, Button, Flex } from "antd";
 
 import "./App.css";
 
 export default function App() {
   const [total, setTotal] = useState(0);
+
+  useEffect(
+    () => {console.log(total)},
+    [total]
+  );
+
   const increase = () => {
     setTotal(total + 1);
   };
@@ -14,17 +20,21 @@ export default function App() {
   return (
     <>
       <div className="App">
-        <Flex gap="small" wrap="wrap">
-          <p>{total}</p>
-        </Flex>
-        <Flex gap="small" wrap="wrap">
-          <Button type="primary" onClick={increase}>
-            Increase
-          </Button>
-          <Button type="primary" onClick={decrease}>
-            Decrease
-          </Button>
-        </Flex>
+        <Space size={16}>
+          <Card title="Counter" style={{ width: 300 }}>
+            <Flex gap="small" wrap="wrap">
+              <p>{total}</p>
+            </Flex>
+            <Flex gap="small" wrap="wrap">
+              <Button type="primary" onClick={increase}>
+                Increase
+              </Button>
+              <Button type="primary" onClick={decrease}>
+                Decrease
+              </Button>
+            </Flex>
+          </Card>
+        </Space>
       </div>
     </>
   );
