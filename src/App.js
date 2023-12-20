677fc590884f8044700479611f44f6573d16b425
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-20 14:14:03 +0300

    added counter buttons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
+  let count = 0;
+  const increase = () => {count ++;}
+  const decrease = () => {count ++;}
   return (
     <>
       <div className="App">
-        <p>gmşlgsşlhşlsfh</p>
+        <p>{count}</p>
+        <button onClick={increase} >increase</button>
+        <button onClick={decrease} >decrease</button>
       </div>
     </>
   );
