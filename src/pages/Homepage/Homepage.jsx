6bbda5fa386877a21ccc346c99e2cbb648d51d66
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-25 14:44:27 +0300

    install axios and init axios.get

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Homepage() {
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
-	.then(response => response.json())
-	.then(json => console.log(json))
-	.catch(err => console.log(err));
-	awaitFetch();
-    }, []);
+    //fetch("https://dummyjson.com/products")
+    //  .then((response) => response.json())
+    //  .then((json) => console.log(json))
+    //  .catch((err) => console.log(err));
+   // awaitFetch();
+    axiosGet();
+  }, []);
 
-	const awaitFetch = async () => {
-		let response = await fetch ("https://dummyjson.com/products");
-		let json = await response.json();
-		console.log(json);
-	}
-  
+ // const awaitFetch = async () => {
+   // let response = await fetch("https://dummyjson.com/products");
+   // let json = await response.json();
+   // console.log(json);
+ // };
+
+  const axiosGet = async () => {
+    let response = await axios.get("https://dummyjson.com/products");
+    console.log(response);
+  };
 
   return (
     <div>
