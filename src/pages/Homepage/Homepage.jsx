77466cbcc2d30c19f2e0d645937a2421dcf53085
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-25 14:37:24 +0300

    created fetch api

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,51 +2,21 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Homepage() {
-  const myAsyncFunction = () => {
-    return new Promise((resolve, reject) => {
-      // async işlem
-      setTimeout(() => {
-        //resolve("veri örneği"); // işlemin başarılı olması
-        reject("istek başarısız"); // işlemin başarısız olması
-      }, 3000);
-    });
-  };
-
   useEffect(() => {
-    // fire and forget
-    //thenCatchFinally();
-    asyncAwait();
-  }, []);
+    fetch("https://dummyjson.com/products")
+	.then(response => response.json())
+	.then(json => console.log(json))
+	.catch(err => console.log(err));
+	awaitFetch();
+    }, []);
 
-  // ES2017 => async, await
+	const awaitFetch = async () => {
+		let response = await fetch ("https://dummyjson.com/products");
+		let json = await response.json();
+		console.log(json);
+	}
+  
 
-  // Vanilla Java, Vanilla C#, Vanilla Javascript
-
-  const asyncAwait = async () => {
-    try {
-      let response = await myAsyncFunction();
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const thenCatchFinally = () => {
-    console.log("loading başladı");
-    myAsyncFunction()
-      .then((response) => {
-        console.log("Bir cevap geldi:", response);
-      })
-      .catch((err) => {
-        console.log("Bir hata geldi:", err);
-      })
-      .finally(() => {
-        console.log("Async işlem sonlandı.");
-        console.log("loading bitti");
-      });
-  };
-  // JS ASYNC
-  // Built-in Promise
   return (
     <div>
       Homepage
